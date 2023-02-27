c605c19d4efac8e65ff1bb4c98e10412d1f53584
Howtonot3 <Pawar.Mothilal341@gmail.com>
2023-02-27 12:37:37 +0530

    rewrote the code and added comments to each query to better understand the query.

--- Foodpurchase SQL analysis/Updated_foodpurchase.sql
+++ Foodpurchase SQL analysis/Updated_foodpurchase.sql
@@ -1,59 +1,109 @@
+-- Exploratory data analysis
 
 USE foodp;
 
--- TABLE : customer_profile
+SHOW TABLES;
 
--- 1. Total records of the dataset
-SELECT COUNT(*) AS total_customers
+SELECT  count(*) FROM customer_profile;
+-- the dataset has records for 2205 customers
+
+SELECT avg(Annual_income) 'avg_income'
+       ,avg(age) 'avg_age'
+       ,ceil(avg(Recency)) 'avg_recency'
 FROM customer_profile;
+-- a typical customer earns 51.6k bucks per year,is 51 years old, and has recency of 50 days.
+
+SELECT count(*) 
+FROM customer_profile
+WHERE Annual_income >= (SELECT avg(Annual_income) FROM customer_profile) ;
+-- nearly 50% of customers earn income above average.
 
 
--- 2. creating view to work with specific columns from the source table
-DROP VIEW  IF EXISTS customer_info;
+DESCRIBE customer_profile;
+DESCRIBE customer_enrol;
 
-CREATE VIEW customer_info 
+-- a view represents the customer details
+
+DROP VIEW IF EXISTS customer_details;
+
+CREATE VIEW customer_details 
 AS
-SELECT customer_id
-	   ,year AS enrollment_year
-	   ,annual_income
-       ,age
-       ,age_group(age) AS age_group
-       ,kids
-       ,teens
-       ,recency
-FROM customer_profile;
+SELECT cp.customer_id
+	   ,Annual_income
+	   ,Age
+       ,kids+teens 'children'
+       ,Recency
+       ,ce.Enrollment_day
+FROM customer_profile cp
+     JOIN
+     customer_enrol ce ON cp.customer_id = ce.customer_id;
+
+-- yearly and monthly enrollments
+
+SELECT year(Enrollment_day) 'Year'
+      ,month(Enrollment_day) 'Month'
+      ,count(customer_id) 'customers-enrolled'
+FROM customer_details
+GROUP BY Year,month
+ORDER BY Year DESC;
+
+-- The enrollment of the customers is consistent over the years
+
+DROP PROCEDURE IF EXISTS yearly_customer_acquit;
+
+DELIMITER $$
+CREATE PROCEDURE yearly_enrollment()
+BEGIN
+      SELECT year(Enrollment_day) 'Year'
+			,count(customer_id) 'customers-enrolled'
+	  FROM customer_details
+	  GROUP BY Year
+	  ORDER BY Year DESC;
+END $$
+DELIMITER ;
+
+call yearly_enrollment;
+-- 8% of customers enrolled in 1905,50% in 1906, and it decreased to 41% in 1907.
+
+DROP VIEW yearly_customer_acquired;
+SHOW TABLES;
+
+-- Average monthly expenditures on various products
+SELECT  year(cd.Enrollment_day) 'Year'
+        ,month(cd.Enrollment_day) 'Month'
+        ,avg(Wines) 'Avg_Wines'
+        ,avg(Fruits) 'Avg_Fruits'
+        ,avg(Meat_products) 'Avg_Meat_products'
+        ,avg(Fish_products) 'Avg_Fish_products'
+        ,avg(Sweet_products) 'Avg_Sweet_products'
+        ,avg(Gold_products) 'Avg_Gold_products'
+        ,avg(Total_exp) 'Avg_total_exp'
+FROM customer_details cd
+	JOIN 
+    product_prefer pp ON cd.customer_id = pp.customer_id
+GROUP BY Year,Month
+ORDER BY Year;
+
+SELECT  age_group(cd.age) 'age_group'
+        ,avg(Wines) 'Avg_Wines'
+        ,avg(Fruits) 'Avg_Fruits'
+        ,avg(Meat_products) 'Avg_Meat_products'
+        ,avg(Fish_products) 'Avg_Fish_products'
+        ,avg(Sweet_products) 'Avg_Sweet_products'
+        ,avg(Gold_products) 'Avg_Gold_products'
+        ,avg(Total_exp) 'Avg_total_exp'
+FROM customer_details cd
+	JOIN 
+    product_prefer pp ON cd.customer_id = pp.customer_id
+GROUP BY age_group
+ORDER BY Avg_total_exp;
 
 
--- 3.Number of customers enrolled for each year.
-SELECT enrollment_year
-       ,COUNT(*) AS total_enrolled_customers
-FROM customer_info
-GROUP BY enrollment_year
-ORDER BY enrollment_year;
+-- Customers spent more on WINES followed by MEAT_PRODUCTS, and then gold,fish_products.Fruits remained as last preference
+-- And total_expenditure on products increased.
 
 
--- 4. Customers with high annual_income and days since their recent purchase
--- Top 10 customers based on annual_income
-
-DROP VIEW IF EXISTS Top_ten_income;
-
-CREATE VIEW Top_ten_income
-AS
-SELECT customer_id
-       ,enrollment_year
-       ,annual_income
-       ,age
-       ,(kids+teens) AS total_children
-       ,recency
-FROM customer_info
-ORDER BY annual_income DESC
-LIMIT 10;
-
-SELECT * FROM Top_ten_income;
-
--- 5. stored function to categorize the age column and then calculate the customers aggregate measurables of all fields
--- we can call the stored function in sql statement
--- Function to categorize customers based on age - 18-25 young_adult,26-60 adult, above 60 as elders
+-- How product preferences varies across different age groups
 
 DROP FUNCTION IF EXISTS age_group;
 
@@ -67,7 +117,7 @@ BEGIN
       DECLARE age_group VARCHAR(20);
       
       IF age > 60 THEN
-             SET age_group = 'Elder';
+             SET age_group = 'elder';
 	  ELSEIF ( age <=60 AND age>25) THEN
 			 SET age_group = 'Adult';
 	  ELSEIF age <= 25 THEN 
@@ -78,95 +128,53 @@ END $$
 DELIMITER ;
 
 
+SELECT age_group(age) 'age_group'
+	   ,count(cd.customer_id) 'customers'
+       ,avg(Annual_income) 'avg_income'
+       ,avg(Recency) 'avg_recency'
+       ,avg(Total_exp) 'avg_expenditure'
+FROM customer_details cd
+   JOIN 
+   product_prefer pp ON cd.customer_id = pp.customer_id
+GROUP BY age_group;
+
+--  approximately 76% of customers are between 25 and 60 years age, 23% are beyond 60 years and less than 25 years constitutes to less than 1%
+-- average income varies across ages and is high for older,adult and then yound_adult
+-- odler and adults are frequent buyers
+
+-- age & compaign,complain and channels
+
+SELECT age_group(age) 'age_group'
+       ,sum(Deal_purchases) 'Deal_purchases'
+       ,sum(Web_purchases) 'Web_purchases'
+       ,sum(Catalogue_purchases) 'Catalogue_purchases'
+       ,sum(Store_purchases) 'Store_purchases'
+       ,sum(Web_visits) 'Web_visits'
+FROM customer_details cd
+   JOIN 
+   channel_perform cp ON cd.customer_id = cp.customer_id
+GROUP BY age_group;
+
+-- As the data dated to 1970s,the customers had large purchases from stores than any other medium followed by web and catalogues.
+-- Deals remained as the last choice for purchasing goods.
 
 
+-- campaign response and complaints registered
 
--- TABLE: product preference
+SELECT age_group(age) 'age_group'
+	  ,round(sum(campaign1)/count(*)*100,2) AS Campaign1
+	  ,round(sum(campaign2)/count(*)*100,2) AS Campaign2
+      ,round(sum(campaign3)/count(*)*100,2) AS Campaign3
+      ,round(sum(campaign4)/count(*)*100,2) AS Campaign4
+      ,round(sum(campaign5)/count(*)*100,2) AS Campaign5
+      ,round(sum(Targeted_response)/count(*)*100,2) AS targetedcamp
+      ,sum(complain) 'complaints'
+FROM customer_details cd
+   JOIN 
+   camp_complain cc ON cd.customer_id = cc.customer_id
+GROUP BY age_group;
 
-CREATE VIEW product_preferred AS
-SELECT ci.annual_income
-      ,ci.enrollment_year
-      ,ci.age_group
-      ,ci.recency
-      ,pp.*
-FROM product_prefer pp
-     JOIN
-	 customer_info ci ON pp.customer_id = ci.customer_id;	
-
- 
- 
- -- 1. yearly pruchases of products
- SELECT enrollment_year
-	  , count(customer_id) AS total_customers
-      ,SUM(wines) AS wines_exp
-      ,SUM(fruits) AS fruits_exp
-      ,SUM(meat_products) AS meat_products_exp
-      ,SUM(fish_products) AS fish_products_exp
-      ,SUM(sweet_products) AS sweet_products_exp
-      ,SUM(gold_products) AS gold_products_exp
-FROM product_preferred
-GROUP BY enrollment_year
-ORDER BY enrollment_year;
-
-
--- 2. percent of total expenditure for each age group
-CREATE VIEW percent_exp
-AS 
-SELECT age_group
-	  ,count(customer_id) AS total_customers
-      ,ROUND(SUM(wines)/SUM(total_exp)*100,2) AS wines_exp
-      ,ROUND(SUM(fruits)/SUM(total_exp)*100,2) AS fruits_exp
-      ,ROUND(SUM(meat_products)/SUM(total_exp)*100,2) AS meat_products_exp
-      ,ROUND(SUM(fish_products)/SUM(total_exp)*100,2) AS fish_products_exp
-      ,ROUND(SUM(sweet_products)/SUM(total_exp)*100,2) AS sweet_products_exp
-      ,ROUND(SUM(gold_products)/SUM(total_exp)*100,2) AS gold_products_exp
-FROM product_preferred
-GROUP BY age_group
-ORDER BY age_group;
-
-
--- wine,meat_products,followed by gold_products are top preferred products among adults and elders. prefer sweets over fruits
--- meat_products are the most preffered products among young adults followed by wines and sweets.Alike adults fruits are least opted products next to gold among young adults
-
-
--- 3. using veiw in stored procedure
-DELIMITER $$
-CREATE PROCEDURE age_group_exp(
-          IN In_age_group VARCHAR(30))
-BEGIN
-     SELECT * FROM percent_exp
-     WHERE age_group = In_age_group;
-     
-END $$
-DELIMITER ;
-
-CALL age_group_exp('young_adult');
- 
--- TABLE - Camp_complain
-
-SELECT * FROM camp_complain;
-
-SELECT round(sum(campaign1)/count(*)*100,2) AS Camp1_response
-	  ,round(sum(campaign2)/count(*)*100,2) AS Camp2_response
-      ,round(sum(campaign3)/count(*)*100,2) AS Camp3_response
-      ,round(sum(campaign4)/count(*)*100,2) AS Camp4_response
-      ,round(sum(campaign5)/count(*)*100,2) AS Camp5_response
-      ,round(sum(Targeted_response)/count(*)*100,2) AS targeted_response
-FROM camp_complain;
-
-
--- No of customers who has zero or more responses for the campaigns
-
-SELECT camp_responses
-	  ,count(customer_id) AS Total_responses
-FROM camp_complain
-GROUP BY Camp_responses;
-
--- the customer who reponsed in target section but not in any other campaigns
-
-SELECT COUNT(customer_id) AS zero_complaint_target_responses
-FROM camp_complain 
-WHERE targeted_response = 1
-AND (campaign1,campaign2,campaign3,campaign4,campaign5) = (0,0,0,0,0);
-
+-- response is consistent over campaigns from older group. Campaign 3 and 5 have more responses from young adult and adults
+-- young adults responsed largely in the targeted campaign while adult and older have equal responses.
+-- adults registered more complaints than older.
 
